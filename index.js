cfecd92f3bfd0458bb5804bf91b83279290ce13e
Deepanshu Pariyani <pariyanideepanshu@gmail.com>
2024-05-28 11:28:54 +0530

    analytics route added

--- index.js
+++ index.js
@@ -3,10 +3,36 @@ const PORT=3000;
 const app=express();
 const urlRoute=require("./routes/url");
 const {connectWithDb}=require("./connection.js");
-
+const URL=require("./models/url");
 connectWithDb("mongodb://localhost:27017/short-url");
 app.use(express.json());
 app.use("/url",urlRoute);
+
+app.get("/getAll",async (req,res)=>{
+    const result = await URL.find({});
+    return res.status(200).json(result);
+});
+
+app.get("/:shortid",async (req,res)=>{
+
+    const shortId=req.params.shortid;
+    const entry = await URL.findOneAndUpdate({
+        shortId
+    },{$push:{
+        visitHistory:{timestamp:Date.now()}
+        }
+    }
+);
+
+res.redirect(entry.redirectUrl);
+});
+
+
+
+
 app.listen(PORT,()=>{
     console.log(`server is started.`)
 });
+
+
+
--- routes/url.js
+++ routes/url.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const router=express.Router();
-const {handleGenerateNewShortUrl}=require("../controllers/url.js");
+const {handleGenerateNewShortUrl,handleGetAnalytics}=require("../controllers/url.js");
 router.post("/",handleGenerateNewShortUrl);
-
+router.get("/analytics/:shortId",handleGetAnalytics);
 
 module.exports=router;
