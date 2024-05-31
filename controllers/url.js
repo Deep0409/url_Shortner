008bc89ab463f9607b1d157fe5d43d629644ed34
Deepanshu Pariyani <pariyanideepanshu@gmail.com>
2024-05-31 19:47:18 +0530

    url generated on UI added

--- controllers/url.js
+++ controllers/url.js
@@ -14,7 +14,7 @@ async function handleGenerateNewShortUrl(req,res){
             visitedHistory:[]
         });
 
-    return res.json({id:shortID});
+    return res.render("home",{id:shortID});
 }
 
 async function handleGetAnalytics(req,res){
